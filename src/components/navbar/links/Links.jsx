7f75fc9136956130c6d12f42e2e4f7f4d20c2fdd
hymndrazill <srihafiras@yahoo.fr>
2024-01-01 23:16:41 +0100

    link components and  css

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import styles from "./links.module.css"
+import NavLink from "./navLink/NavLink";
+const Links = () => {
+    const links = [
+        {title: "HomePage" ,path : "/"},
+        {title:"About",path : "/about"},
+        {title:"Contact",path : "/contact"},
+        {title:"Blog",path : "/blog"},
+    ];
+
+
+   
+  return (
+    <div className={styles.links}>
+        {links.map((link,index) => (
+         <NavLink item={link} key={index}/>
+                 ))}
+
+    </div>
+  )
+
+  
+
+}
+
+export default Links
