7070b964b5c0707dfb252d40f9f5bab47c7083c8
hymndrazill <srihafiras@yahoo.fr>
2024-01-05 11:58:54 +0100

    debugging

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,12 +1,44 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials"
 import { connectToDb } from "./utils";
 import { User } from "./models";
+import credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt"
+const login = async (credentials) => {
+  try {
+    connectToDb();
+    const user = await User.findOne({username: credentials.username});
+    if(!user) {
+      throw new Error("Wrong credentials")
+    }
+    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+    if(!isPasswordCorrect){
+      throw new Error("wrong credentials")
+    }
+    return user
+  } catch (error) {
+      console.log(error);
+      throw new Error("failed ")
+  }
+}
+
+
 export const {handlers:{GET,POST}, auth,signIn,signOut}  =  NextAuth({
   providers: [
-    GitHub({
+    GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CredentialsProvider({
+      async authorize(credentials) {
+        try {
+          const user = await login(credentials)
+          return user
+        } catch (error) {
+          return null;
+        }
+      }
     })
     ],
     callbacks:{
